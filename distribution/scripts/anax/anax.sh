4b7d2435f698552b0eba0806075841c9551fa92b
michael dye <m-github@divisive.info>
2016-11-02 17:38:18 -0600

    added flexible config sourcing using defaults, moved to v1.6.17

--- distribution/scripts/anax/anax.sh
+++ distribution/scripts/anax/anax.sh
@@ -18,21 +18,31 @@ if ! [ "$(ls -A $SNAP_DATA/)" ]; then
     ln -s $SNAP/contracts $C_PATH/
 fi
 
-# for chain selection convenience
-if [ -e "/boot/firmware/horizon_directory_version" ]; then
-  cp /boot/firmware/horizon_directory_version $SNAP_DATA/anax/directory_version
-fi
-
-export CMTN_DIRECTORY_VERSION=${CMTN_DIRECTORY_VERSION:=$(cat $SNAP_DATA/anax/directory_version)}
-
-export CMTN_WHISPER_ADDRESS_PATH=${CMTN_WHISPER_ADDRESS_PATH:=$SNAP_COMMON/eth/shhid}
+function sourceOverrideOrDefaultPath() {
+  if [ -e "${SNAP_COMMON}/config/$1" ]; then
+    echo "${SNAP_COMMON}/config/$1"
+  else
+    echo "$SNAP_DATA/anax/default-$1"
+  fi
+}
 
 # for go-solidity contract loading
 export GOPATH=$SNAP_DATA/go
 
+# path for state saved by app
+export CMTN_WHISPER_ADDRESS_PATH=${CMTN_WHISPER_ADDRESS_PATH:=$SNAP_COMMON/eth/shhid}
+
+# envvars used by go-solidity lib
+export mtn_soliditycontract_block_read_delay=${mtn_soliditycontract_block_read_delay:=3}
+
+export CMTN_DIRECTORY_VERSION=${CMTN_DIRECTORY_VERSION:=$(cat $(sourceOverrideOrDefaultPath "directory_version"))}
+
+export CMTN_DEVICE_ID=${CMTN_DEVICE_ID:=$(cat $SNAP_COMMON/config/device_id)}
+
+# anax envvars
 ANAX_LOG_LEVEL=${ANAX_LOG_LEVEL:=3}
 
-# for help debugging startup
-env
+# write out rendered config file for investigation later
+$(dirname $0)/render-config.sh $(sourceOverrideOrDefaultPath "anax.json.tmpl") > ${SNAP_DATA}/anax/rendered-config.json
 
-anax -v=$ANAX_LOG_LEVEL -alsologtostderr -config <($(dirname $0)/render-config.sh $SNAP_DATA/anax/config.tmpl)
+anax -v=$ANAX_LOG_LEVEL -alsologtostderr -config $SNAP_DATA/anax/rendered-config.json
