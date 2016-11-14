de66eb8c9fbf0110ac486fb06a1f1950435b355d
michael dye <m-github@divisive.info>
2016-11-14 10:55:22 -0700

    updated to ethbuilds 5025230778c17f0013a9626d475b61d9d4b33963

--- ethereum/hex2string.c
+++ ethereum/hex2string.c
@@ -9,20 +9,21 @@ char nibbletochar(char c)
 		return c - 'a' + 10;
 	if (c >= 'A' && c <= 'F')
 		return c - 'A' + 10;
+
 	return 255;
 }
 
 int main(int argv, char *argc[])
 {
-	unsigned int i;
-	unsigned char x;
+	unsigned int i, j;
 
-	if (argv == 2)
-		for (i = 0; i < strlen(argc[1]); i++) {
-			x = nibbletochar(argc[1][i++]) << 4;
-			x += nibbletochar(argc[1][i]);
-			printf("%c", x);
-		}
+	if (argv != 2)
+		return 1;
+
+	j = (strlen(argc[1])/2) << 1;
+
+	for (i = 0; i < j; i+=2)
+		printf("%c", (char) (nibbletochar(argc[1][i]) << 4) | nibbletochar(argc[1][i+1]));
 
 	return 0;
 }
--- ethereum/string2hex.c
+++ ethereum/string2hex.c
@@ -5,9 +5,11 @@ int main(int argv, char *argc[])
 {
 	unsigned int i;
 
-	if (argv == 2)
-		for (i = 0; i < strlen(argc[1]); i++)
-			printf("%02x", argc[1][i]);
+	if (argv != 2)
+		return 1;
+
+	for (i = 0; i < strlen(argc[1]); i++)
+		printf("%02x", argc[1][i]);
 
 	return 0;
 }
