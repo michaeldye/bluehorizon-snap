4c9014eb31b52af8054daaa960aed497a2cf6ef4
linggao <linggao@us.ibm.com>
2016-12-09 01:37:54 +0000

    fixed the HORIZON_WORKLOAD_CPUSET env variable for anax config

--- distribution/scripts/anax/render-config.sh
+++ distribution/scripts/anax/render-config.sh
@@ -9,8 +9,10 @@
 if ! [ -n "$HORIZON_WORKLOAD_CPUSET" ]; then
   # N.B.: this is a little naive, it includes HT threads
   NUM=$(nproc)
-  if [[ $NUM > 0 ]]; then
+  if [[ $NUM -gt 2 ]]; then
     export HORIZON_WORKLOAD_CPUSET="1-$((NUM-1))"
+  elif [[ $NUM -eq 2 ]]; then
+    export HORIZON_WORKLOAD_CPUSET="1"
   else
     export HORIZON_WORKLOAD_CPUSET="0"
   fi
